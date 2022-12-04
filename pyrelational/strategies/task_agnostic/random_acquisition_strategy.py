3850b6d299a0cd206f047b0742b130e29bb051e6
paulmorio <pms69@cam.ac.uk>
2022-12-04 06:30:06 +0000

    Updated task agnostic strategies to use new pipeline API changes

--- pyrelational/strategies/task_agnostic/random_acquisition_strategy.py
+++ pyrelational/strategies/task_agnostic/random_acquisition_strategy.py
@@ -5,16 +5,15 @@ from typing import List
 import numpy as np
 
 from pyrelational.data import GenericDataManager
-from pyrelational.models import GenericModel
 from pyrelational.strategies.generic_al_strategy import GenericActiveLearningStrategy
 
 
 class RandomAcquisitionStrategy(GenericActiveLearningStrategy):
     """Implements RandomAcquisition whereby random samples from unlabelled set are chosen at each step"""
 
-    def __init__(self, data_manager: GenericDataManager, model: GenericModel):
-        super(RandomAcquisitionStrategy, self).__init__(data_manager, model)
+    def __init__(self):
+        super(RandomAcquisitionStrategy, self).__init__()
 
-    def active_learning_step(self, num_annotate: int) -> List[int]:
-        num_annotate = min(num_annotate, len(self.u_indices))
-        return np.random.choice(self.u_indices, size=num_annotate, replace=False).tolist()
+    def active_learning_step(self, num_annotate: int, data_manager: GenericDataManager) -> List[int]:
+        num_annotate = min(num_annotate, len(data_manager.u_indices))
+        return np.random.choice(data_manager.u_indices, size=num_annotate, replace=False).tolist()
--- pyrelational/strategies/task_agnostic/relative_distance_strategy.py
+++ pyrelational/strategies/task_agnostic/relative_distance_strategy.py
@@ -4,21 +4,20 @@ import torch
 
 from pyrelational.data import GenericDataManager
 from pyrelational.informativeness import relative_distance
-from pyrelational.models import GenericModel
 from pyrelational.strategies.generic_al_strategy import GenericActiveLearningStrategy
 
 
 class RelativeDistanceStrategy(GenericActiveLearningStrategy):
     """Diversity sampling based active learning strategy"""
 
-    def __init__(
-        self,
-        data_manager: GenericDataManager,
-        model: GenericModel,
-    ):
-        super(RelativeDistanceStrategy, self).__init__(data_manager, model)
+    def __init__(self):
+        super(RelativeDistanceStrategy, self).__init__()
 
-    def active_learning_step(self, num_annotate: int, metric: str = "euclidean") -> List[int]:
-        scores = relative_distance(self.u_loader, self.l_loader, metric=metric)
+    def active_learning_step(
+        self, num_annotate: int, data_manager: GenericDataManager, metric: str = "euclidean"
+    ) -> List[int]:
+        scores = relative_distance(
+            data_manager.get_unlabelled_loader(), data_manager.get_labelled_loader(), metric=metric
+        )
         ixs = torch.argsort(scores, descending=True).tolist()
-        return [self.u_indices[i] for i in ixs[:num_annotate]]
+        return [data_manager.u_indices[i] for i in ixs[:num_annotate]]
