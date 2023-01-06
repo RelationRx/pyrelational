82ec08a109618eb5d56300b0ee4af10b9fbac899
paulmorio <pms69@cam.ac.uk>
2023-01-06 17:34:31 +0000

    Ensemble uncertainty classification and lightning diversity classification example

--- examples/demo/ensemble_uncertainty_classification.py
+++ examples/demo/ensemble_uncertainty_classification.py
@@ -5,11 +5,10 @@ task with the active learning library
 It illustrates the ensemble method.
 """
 
+# User imports
 import logging
 
 import torch
-
-# Pytorch
 from torchvision import datasets, transforms
 
 # Dataset and machine learning model
@@ -18,12 +17,13 @@ from examples.utils.ml_models import MnistClassification
 # Active Learning package
 from pyrelational.data import DataManager
 from pyrelational.models import LightningEnsembleModel
+from pyrelational.oracle import BenchmarkOracle
+from pyrelational.pipeline import Pipeline
 from pyrelational.strategies.classification import LeastConfidenceStrategy
 
 # dataset
 dataset = datasets.FashionMNIST(root="data", train=True, download=True, transform=transforms.ToTensor())
-
-dataset = [dataset[i] for i in range(10000)]
+dataset = torch.utils.data.Subset(dataset, indices=range(10000))
 
 train_ds, val_ds, test_ds = torch.utils.data.random_split(dataset, [9000, 500, 500])
 train_indices = train_ds.indices
@@ -32,7 +32,7 @@ test_indices = test_ds.indices
 
 # model
 model = LightningEnsembleModel(
-    model_class=MnistClassification, model_config={}, trainer_config={"epochs": 4}, n_estimators=5
+    model_class=MnistClassification, model_config={}, trainer_config={"epochs": 5}, n_estimators=5
 )
 
 # data_manager and defining strategy
@@ -44,18 +44,23 @@ data_manager = DataManager(
     loader_batch_size=1000,
 )
 
-strategy = LeastConfidenceStrategy(data_manager=data_manager, model=model)
+# Set up active learning pipeline
+strategy = LeastConfidenceStrategy()
+oracle = BenchmarkOracle()
+pipeline = Pipeline(data_manager=data_manager, model=model, strategy=strategy, oracle=oracle)
 
 # Remove lightning prints
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
-# performance with the full trainset labelled
-strategy.theoretical_performance()
+# See performance with the full trainset labelled
+pipeline.theoretical_performance()
 
 # New data to be annotated, followed by an update of the data_manager and model
-to_annotate = strategy.active_learning_step(num_annotate=1000)
-strategy.active_learning_update(indices=to_annotate, update_tag="Manual Update")
+to_annotate = pipeline.active_learning_step(num_annotate=1000)
+pipeline.active_learning_update(indices=to_annotate, update_tag="Manual Update")
 
 # Annotating data step by step until the trainset is fully annotated
-strategy.full_active_learning_run(num_annotate=1000)
-print(strategy)
+pipeline.full_active_learning_run(num_annotate=1000)
+
+# Pretty printed summary of the components in the pipeline along with annotation/performance history
+print(pipeline)
--- examples/demo/lightning_diversity_classification.py
+++ examples/demo/lightning_diversity_classification.py
@@ -16,18 +16,20 @@ from examples.utils.ml_models import BreastCancerClassification  # noqa: E402
 # Active Learning package
 from pyrelational.data import DataManager
 from pyrelational.models import LightningModel
+from pyrelational.oracle import BenchmarkOracle
+from pyrelational.pipeline import Pipeline
 from pyrelational.strategies.task_agnostic.relative_distance_strategy import (
     RelativeDistanceStrategy,
 )
 
-# dataset
+# Obtain dataset and set up labelled and unlabelled subsets
 dataset = BreastCancerDataset()
 train_ds, val_ds, test_ds = torch.utils.data.random_split(dataset, [500, 30, 39])
 train_indices = train_ds.indices
 val_indices = val_ds.indices
 test_indices = test_ds.indices
 
-# model
+# Instantiate model
 model = LightningModel(model_class=BreastCancerClassification, model_config={}, trainer_config={"epochs": 4})
 
 # data_manager and defining strategy
@@ -39,18 +41,23 @@ data_manager = DataManager(
     hit_ratio_at=5,
 )
 
-strategy = RelativeDistanceStrategy(data_manager=data_manager, model=model)
+# Setup
+strategy = RelativeDistanceStrategy()
+oracle = BenchmarkOracle()
+pipeline = Pipeline(data_manager=data_manager, model=model, strategy=strategy, oracle=oracle)
 
 # Remove lightning prints
 logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
 # performance with the full trainset labelled
-strategy.theoretical_performance()
+pipeline.theoretical_performance()
 
 # New data to be annotated, followed by an update of the data_manager and model
-to_annotate = strategy.active_learning_step(num_annotate=100)
-strategy.active_learning_update(indices=to_annotate, update_tag="Manual Update")
+to_annotate = pipeline.active_learning_step(num_annotate=100)
+pipeline.active_learning_update(indices=to_annotate, update_tag="Manual Update")
 
 # Annotating data step by step until the trainset is fully annotated
-strategy.full_active_learning_run(num_annotate=100)
-print(strategy)
+pipeline.full_active_learning_run(num_annotate=100)
+
+# Pretty printed summary of the components in the pipeline along with annotation/performance history
+print(pipeline)
