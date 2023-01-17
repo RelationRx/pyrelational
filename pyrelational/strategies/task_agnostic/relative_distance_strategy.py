c50692ff922563960a171fea1ff3f7076a730984
thomasgaudelet <69150449+thomasgaudelet@users.noreply.github.com>
2023-01-17 17:39:56 +0000

    move `metric` from method input to class input in RelativeDistanceStrategy (#59)

--- pyrelational/strategies/task_agnostic/relative_distance_strategy.py
+++ pyrelational/strategies/task_agnostic/relative_distance_strategy.py
@@ -10,12 +10,18 @@ from pyrelational.strategies.abstract_strategy import Strategy
 class RelativeDistanceStrategy(Strategy):
     """Diversity sampling based active learning strategy"""
 
-    def __init__(self):
-        super(RelativeDistanceStrategy, self).__init__()
+    def __init__(self, metric: str = "euclidean"):
+        """
+        Initialize module.
 
-    def __call__(self, num_annotate: int, data_manager: DataManager, metric: str = "euclidean") -> List[int]:
+        :param metric: name of distance metric to use.
+        """
+        super(RelativeDistanceStrategy, self).__init__()
+        self.metric = metric
+
+    def __call__(self, num_annotate: int, data_manager: DataManager) -> List[int]:
         scores = relative_distance(
-            data_manager.get_unlabelled_loader(), data_manager.get_labelled_loader(), metric=metric
+            data_manager.get_unlabelled_loader(), data_manager.get_labelled_loader(), metric=self.metric
         )
         ixs = torch.argsort(scores, descending=True).tolist()
         return [data_manager.u_indices[i] for i in ixs[:num_annotate]]
--- tests/strategies/agnostic_strategy_test_cases.py
+++ tests/strategies/agnostic_strategy_test_cases.py
@@ -7,24 +7,21 @@ from pyrelational.strategies.task_agnostic import (
 )
 
 TASK_AGNOSTIC_TEST_CASES = [
-    {"task_type": "regression", "strategy_class": RandomAcquisitionStrategy, "strategy_kwargs": {}, "step_kwargs": {}},
-    {"task_type": "regression", "strategy_class": RelativeDistanceStrategy, "strategy_kwargs": {}, "step_kwargs": {}},
+    {"task_type": "regression", "strategy_class": RandomAcquisitionStrategy, "strategy_kwargs": {}},
+    {"task_type": "regression", "strategy_class": RelativeDistanceStrategy, "strategy_kwargs": {}},
     {
         "task_type": "regression",
         "strategy_class": RelativeDistanceStrategy,
-        "strategy_kwargs": {},
-        "step_kwargs": {"metric": "cosine"},
+        "strategy_kwargs": {"metric": "cosine"},
     },
     {
         "task_type": "regression",
         "strategy_class": RepresentativeSamplingStrategy,
         "strategy_kwargs": {"clustering_method": "AffinityPropagation"},
-        "step_kwargs": {},
     },
     {
         "task_type": "regression",
         "strategy_class": RepresentativeSamplingStrategy,
         "strategy_kwargs": {"clustering_method": AgglomerativeClustering(n_clusters=10)},
-        "step_kwargs": {},
     },
 ]
