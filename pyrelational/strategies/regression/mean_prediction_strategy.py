4c03ddf95c9bc27d62e9668c6048d0dc9c521f99
alicerdv <77078028+alicerdv@users.noreply.github.com>
2023-02-07 06:01:36 -0500

    fix docstring

--- pyrelational/strategies/regression/mean_prediction_strategy.py
+++ pyrelational/strategies/regression/mean_prediction_strategy.py
@@ -7,8 +7,8 @@ from pyrelational.strategies.regression.abstract_regression_strategy import (
 
 
 class MeanPredictionStrategy(RegressionStrategy):
-    """Implements Me Strategy whereby unlabelled samples are queried based on their predicted mean value
-    by the model"""
+    """Implements Mean Prediction Strategy whereby unlabelled samples are queried based on their predicted mean value
+    by the model. ie samples with the highest predicted mean values are queried."""
 
     def scoring_function(self, predictions: Tensor) -> Tensor:
         return regression_mean_prediction(predictions)
