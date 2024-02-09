c00b5320f8bd6ff963a7f25e45fb34ae363e10c6
thomasgaudelet <thomas@relationrx.com>
2024-02-09 14:00:02 +0000

    fix lint

--- pyrelational/model_managers/model_utils.py
+++ pyrelational/model_managers/model_utils.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict
 
 import torch
-from lightning.pytorch.trainer.connectors.accelerator_connector import _AcceleratorConnector
+from lightning.pytorch.trainer.connectors.accelerator_connector import (
+    _AcceleratorConnector,
+)
 
 
 def _determine_device(trainer_config: Dict[str, Any]) -> torch.device:
@@ -9,10 +11,10 @@ def _determine_device(trainer_config: Dict[str, Any]) -> torch.device:
     Determines the torch device of the model from the gpus argument for pytorch lightning trainer
 
     :param trainer_config: configuration dictionary for a pytorch lightning Trainer
-    :return: torch device
+    :return: torch device object
     """
     accelerator = _AcceleratorConnector(
-            accelerator=trainer_config.get("accelerator", "cpu"),
-            devices=trainer_config.get("devices", "auto")
-        )
-    return accelerator.strategy.root_device
+        accelerator=trainer_config.get("accelerator", "cpu"), devices=trainer_config.get("devices", "auto")
+    )
+    device: torch.device = accelerator.strategy.root_device
+    return device
