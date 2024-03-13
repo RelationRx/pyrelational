9865cf322d63c345735770a55203ddc0f9ce8611
Philippe <philippe.bergna@advai.co.uk>
2024-03-13 17:03:39 +0000

    feat(object-detection): extend the __init__.py to initialise the object detection informative strategies

--- pyrelational/informativeness/__init__.py
+++ pyrelational/informativeness/__init__.py
@@ -1,3 +1,4 @@
 from pyrelational.informativeness.classification import *  # noqa
 from pyrelational.informativeness.regression import *  # noqa
 from pyrelational.informativeness.task_agnostic import *  # noqa
+from pyrelational.informativeness.object_detection import *  # noqa
