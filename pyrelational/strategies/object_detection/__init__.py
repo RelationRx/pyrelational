736ab34ddbb5cc10c0f3ed904e5bde7e83cce08d
Philippe <philippe.bergna@advai.co.uk>
2024-03-13 16:51:47 +0000

    feat(object-detection): initialize object detection strategies in __init__.py
    - Implement __init__.py for object detection strategies, enabling streamlined imports of EntropyObjectDetectionStrategy, LeastConfidenceStrategy, and MarginalConfidenceStrategy. This enhances the modularity and ease of access to different active learning strategies for object detection within PyRelationAL.

---
+++ pyrelational/strategies/object_detection/__init__.py
@@ -0,0 +1,9 @@
+from pyrelational.strategies.object_detection.entropy_object_detection_strategy import (
+    EntropyObjectDetectionStrategy,
+)
+from pyrelational.strategies.object_detection.least_confidence_strategy import (
+    LeastConfidenceStrategy,
+)
+from pyrelational.strategies.object_detection.marginal_confidence_strategy import (
+    MarginalConfidenceStrategy,
+)
