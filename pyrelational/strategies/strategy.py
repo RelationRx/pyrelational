d1b7ac224881fcbed8200326bc5161eda88aa12d
thomasgaudelet <thomas@relationrx.com>
2024-08-09 16:52:37 +0100

    fix generic strategy docstring

--- pyrelational/strategies/strategy.py
+++ pyrelational/strategies/strategy.py
@@ -1,3 +1,5 @@
+"""Flexible active learning strategy."""
+
 from typing import Any, List
 
 from pyrelational.data_managers import DataManager
@@ -8,27 +10,26 @@ from pyrelational.strategies.abstract_strategy import Strategy
 
 
 class GenericStrategy(Strategy):
-    """
-    This module defines an abstract active learning strategy.
+    """This module can be used to define flexible active learning strategy.
 
-    Any strategy should be a subclass of this class and override the `__call__` method to suggest observations
-    to be labeled. In the general case `__call__` would be the composition of an informativeness function,
-    which assigns a measure of informativeness to unlabelled observations, and a selection algorithm which
-    chooses what observations to present to the oracle.
-
-    The user defined __call__ method must have a "num_annotate" argument
+    This relies on the assumption that (most) active learning strategies can be
+    decomposed into two components: a scoring function and a sampler. Thus to initialize
+    this class, simply pass a scorer and a sampler.
     """
 
     def __init__(self, scorer: AbstractScorer, sampler: AbstractSampler):
+        """Initialize the strategy with a scorer and a sampler.
+
+        :param scorer: instance of a scorer class
+        :param sampler: instance of a sampler class
+        """
         self.scorer = scorer
         self.sampler = sampler
 
     def __call__(
         self, num_annotate: int, data_manager: DataManager, model_manager: ModelManager[Any, Any]
     ) -> List[int]:
-        """
-        Call function which identifies samples which need to be labelled based on
-        user defined scoring function.
+        """Call function which identifies samples which need to be labelled based on user defined scoring function.
 
         :param num_annotate: number of samples to annotate
         :param data_manager: A pyrelational data manager
