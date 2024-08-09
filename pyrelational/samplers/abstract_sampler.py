3986d7b341e4e93a67b706d28f8c419919f895c5
thomasgaudelet <thomas@relationrx.com>
2024-08-09 16:34:04 +0100

    samplers docstrings

--- pyrelational/samplers/abstract_sampler.py
+++ pyrelational/samplers/abstract_sampler.py
@@ -1,40 +1,43 @@
+"""Collection of samplers defining AL strategies."""
+
 from abc import ABC, abstractmethod
-from typing import Any, List
+from typing import List
 
 import torch
 from torch import Tensor
 
 
 class AbstractSampler(ABC):
-    """_summary_"""
+    """Abstract sampler class."""
 
     @abstractmethod
     def __call__(self, scores: Tensor, indices: List[int], num_samples: int) -> List[int]:
-        """_summary_
+        """Sample a subset of indices based on the scores.
 
-        :return: _description_
+        This method should be implemented by the subclasses.
+        :return: List of sampled indices.
         """
         pass
 
 
 class DeterministicSampler(AbstractSampler):
-    """_summary_"""
+    """Deterministic sampler based on the top-k scores."""
 
     def __call__(self, scores: Tensor, indices: List[int], num_samples: int) -> List[int]:
-        """_summary_
+        """Sample the top-k indices based on the scores.
 
-        :return: _description_
+        :return: List of sampled indices.
         """
         ixs = torch.argsort(scores, descending=True).tolist()
         return [indices[i] for i in ixs[:num_samples]]
 
 
 class ProbabilisticSampler(AbstractSampler):
-    """_summary_"""
+    """Probabilistic sampler based on the scores."""
 
     def __call__(self, scores: Tensor, indices: List[int], num_samples: int) -> List[int]:
-        """_summary_
+        """Sample a subset of indices deriving a distribution from the scores.
 
-        :return: _description_
+        :return: List of sampled indices.
         """
         return [indices[i] for i in torch.multinomial(scores, num_samples, replacement=False).tolist()]
