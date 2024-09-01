f55d79c72bb0814ff063deae41210112205aab94
paulmorio <pms69@cam.ac.uk>
2024-09-01 19:25:53 +0100

    Airfoil dataset

---
+++ benchmarking/airfoil.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch
+
+# Scikit learn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import balanced_accuracy_score, auc, roc_auc_score
+
+# Data and data manager
+from pyrelational.datasets.regression.uci import UCIAirfoil
+from pyrelational.data_managers import DataManager
+
+# Model, strategy, oracle, and pipeline
+from pyrelational.oracles import BenchmarkOracle
+from pyrelational.pipeline import Pipeline
+
+# Ray Tune
+from ray import tune
+from ray.train import RunConfig
+import os
+
+from regression_experiment_utils import get_strategy_from_string, numpy_collate
+from regression_experiment_utils import GPR
+from regression_experiment_utils import experiment_param_space
+from ray_functions import set_all_seeds
+
+def get_airfoil_data_manager():
+    ds = UCIAirfoil()
+    train_ds, valid_ds, test_ds = torch.utils.data.random_split(ds, [1000, 100, 402])
+    train_indices = train_ds.indices
+    valid_indices = valid_ds.indices
+    test_indices = test_ds.indices
+
+    return DataManager(
+        ds,
+        train_indices=train_indices,
+        validation_indices=valid_indices,
+        test_indices=test_indices,
+        labelled_indices=np.random.choice(train_indices, 10, replace=False).tolist(),
+        loader_batch_size="full",
+        loader_collate_fn=numpy_collate,
+    )
+
+config = {
+    "seed": 42,
+    "strategy": "upper_confidence_bound"
+}
+
+def trial(config):
+    seed = config["seed"]
+    set_all_seeds(seed)
+    strategy = get_strategy_from_string(config["strategy"])
+    data_manager = get_airfoil_data_manager()
+    model_config = {}
+    trainer_config = {}
+    model_manager = GPR(model_config, trainer_config)
+    oracle = BenchmarkOracle()
+    pipeline = Pipeline(data_manager=data_manager, model_manager=model_manager, strategy=strategy, oracle=oracle)
+
+    # Annotating data step by step until the trainset is fully annotated
+    pipeline.run(num_annotate=1)
+    print(pipeline)
+
+    iteration_metrics = []
+    for i in range(len(pipeline.performances)):
+        if "test_metric" in pipeline.performances[i]:
+            iteration_metrics.append(pipeline.performances[i]["test_metric"])
+
+    iteration_metrics = np.array(iteration_metrics)
+    score_area_under_curve = auc(np.arange(len(iteration_metrics)), iteration_metrics)
+
+    return {"score": score_area_under_curve, "iteration_metrics": iteration_metrics}
+
+# Configure and specift the tuner which will run the trials
+experiment_name = "airfoil"
+storage_path = os.path.join(os.getcwd(), "benchmark_results")
+
+trial = tune.with_resources(trial, {"cpu": 3})
+tuner = tune.Tuner(
+    trial,
+    tune_config=tune.TuneConfig(num_samples=1),
+    param_space=experiment_param_space,
+    run_config = RunConfig(
+        name = experiment_name,
+        storage_path = storage_path,)
+)
+results = tuner.fit()
