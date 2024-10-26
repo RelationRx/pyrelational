cf0b2958c4d496ab3bf47a46b2f83f0f1a044f87
paulmorio <paul.scherer@relationrx.com>
2024-10-26 22:29:41 +0100

    Wine benchmarking

---
+++ benchmarking/wine/data_manager.py
@@ -0,0 +1,37 @@
+"""Benchmarking DataManager for the wine dataset
+"""
+
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
+
+import numpy as np
+import torch
+from numpy.typing import NDArray
+
+from pyrelational.data_managers import DataManager
+from pyrelational.datasets.regression.uci import UCIWine
+
+
+def get_wine_data_manager() -> DataManager:
+    ds = UCIWine()
+    print(len(ds))
+    train_ds, valid_ds, test_ds = torch.utils.data.random_split(ds, [1000, 100, 498])
+    train_indices = list(train_ds.indices)
+    valid_indices = list(valid_ds.indices)
+    test_indices = list(test_ds.indices)
+
+    return DataManager(
+        ds,
+        train_indices=train_indices,
+        validation_indices=valid_indices,
+        test_indices=test_indices,
+        labelled_indices=np.random.choice(train_indices, 1, replace=False).tolist(),
+        loader_batch_size="full",
+        loader_collate_fn=numpy_collate,
+    )
+
+
+def numpy_collate(
+    batch: List[Union[torch.Tensor, NDArray[Union[Any, np.float32, np.float64]]]]
+) -> List[NDArray[Union[Any, np.float32, np.float64]]]:
+    """Collate function for a Pytorch to Numpy DataLoader"""
+    return [np.stack([b.numpy() if isinstance(b, torch.Tensor) else b for b in samples]) for samples in zip(*batch)]
