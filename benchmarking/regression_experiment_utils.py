5caa332836bc0ed7144180c6282a27fbc93f7f14
paulmorio <paul.scherer@relationrx.com>
2024-10-30 12:23:18 +0000

    Regression utils with scikit ensemble

--- benchmarking/regression_experiment_utils.py
+++ benchmarking/regression_experiment_utils.py
@@ -1,3 +1,4 @@
+# type: ignore
 """
 Utility functions for scripting Active learning benchmark experiments where the model is a regressor.
 """
@@ -17,6 +18,9 @@ from ray.train import RunConfig
 # Scikit learn
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
 from torch.nn import Module
 from torch.utils.data import DataLoader
 
@@ -53,13 +57,6 @@ def get_strategy_from_string(strategy: str) -> Any:
         return RandomAcquisitionStrategy()
     else:
         raise ValueError("Invalid strategy")
-
-
-# def numpy_collate(
-#     batch: List[Union[torch.Tensor, NDArray[Union[Any, np.float32, np.float64]]]]
-# ) -> List[NDArray[Union[Any, np.float32, np.float64]]]:
-#     """Collate function for a Pytorch to Numpy DataLoader"""
-#     return [np.stack(el) for el in zip(*batch)]
 
 
 def numpy_collate(
@@ -108,6 +105,80 @@ class GPR(ModelManager[GaussianProcessRegressor, GaussianProcessRegressor]):
             return torch.FloatTensor(class_probabilities).T
 
 
+class EnsembleScikit(ModelManager[Any, Any]):
+    """
+    Class for constructing ModelManager managing ensembles of models implemented
+    in Scikit-Learn and interfacing with PyRelationAL.
+
+    Args:
+        model_class (sklearn estimator): Estimator that should be ensembled (e.g. MLPRegressor)
+        num_estimators (int): number of estimators in the ensemble
+        model_config (dict): dictionary containing any model_class specific arguments
+        trainer_config (dict): dictionary containing any taining specific arguments
+    """
+
+    def __init__(
+        self,
+        model_class: Union[Any, MLPRegressor],
+        num_estimators: int,
+        model_config: Dict[str, Any],
+        trainer_config: Dict[str, Any],
+    ):
+        super(EnsembleScikit, self).__init__(model_class, model_config, trainer_config)
+        self.model_config = model_config
+        self.trainer_config = trainer_config
+        self.num_estimators = num_estimators
+
+    def train(
+        self, train_loader: DataLoader[Any], valid_loader: Optional[DataLoader[Any]] = None, bagging: bool = False
+    ) -> None:
+        train_x, train_y = next(iter(train_loader))
+        estimators = []
+
+        if train_x.shape[0] > self.num_estimators:
+            bagging = True
+
+        if bagging:
+            k = self.num_estimators
+            kf = KFold(n_splits=k, shuffle=True, random_state=self.model_config["random_state"])
+            from joblib import Parallel, delayed
+
+            estimators = Parallel(n_jobs=-1)(
+                delayed(self._init_model().fit)(train_x[train_index], train_y[train_index])
+                for train_index, _ in kf.split(train_x)
+            )
+        else:
+            from joblib import Parallel, delayed
+
+            estimators = Parallel(n_jobs=-1)(
+                delayed(self._init_model().fit)(train_x, train_y) for _ in range(self.num_estimators)
+            )
+        self._current_model = estimators
+
+    def test(self, loader: DataLoader[Any]) -> Dict[str, float]:
+        if not self.is_trained():
+            raise ValueError("No current model, call 'train(X, y)' to train the model first")
+        X, y = next(iter(loader))
+        scores = []
+        for idx in range(self.num_estimators):
+            estimator = self._current_model[idx]
+            predictions = estimator.predict(X)
+            score = mean_squared_error(y, predictions)
+            scores.append(score)
+        return {"test_metric": np.mean(scores)}
+
+    def __call__(self, loader: DataLoader[Any]) -> Any:
+        if not self.is_trained():
+            raise ValueError("No current model, call 'train(X, y)' to train the model first")
+        X, _ = next(iter(loader))
+        predictions = []  # list of num_estimator predictions of shape y
+        for est_idx in range(self.num_estimators):
+            estimator = self._current_model[est_idx]
+            predictions.append(torch.FloatTensor(estimator.predict(X)))
+        predictions = torch.vstack(predictions)
+        return predictions
+
+
 experiment_param_space = {
     "seed": tune.grid_search([1, 2, 3, 4, 5]),
     "strategy": tune.grid_search(
@@ -116,8 +187,8 @@ experiment_param_space = {
             "expected_improvement",
             "greedy",
             "thompson_sampling",
-            "upper_confidence_bound",
-            "variance_reduction",
+            # "upper_confidence_bound",
+            # "variance_reduction",
             "random",
         ]
     ),
