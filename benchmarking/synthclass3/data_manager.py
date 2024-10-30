17cf9b3d8f7553e3225edd8ef3d33eacd4b8f41d
paulmorio <paul.scherer@relationrx.com>
2024-10-30 13:07:37 +0000

    SynthClass

---
+++ benchmarking/synthclass3/data_manager.py
@@ -0,0 +1,42 @@
+# type: ignore
+
+"""Benchmarking DataManager for the SynthClass3 dataset
+"""
+import random
+import time
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
+
+import numpy as np
+import torch
+from numpy.typing import NDArray
+
+from pyrelational.data_managers import DataManager
+from pyrelational.datasets.classification.synthetic import SynthClass3
+
+from ..classification_experiment_utils import (
+    make_class_stratified_train_val_test_split,
+    pick_one_sample_per_class,
+)
+
+
+def get_synthclass3_data_manager(seed: int) -> DataManager:
+    ds = SynthClass3(random_seed=seed)
+    train_indices, valid_indices, test_indices = make_class_stratified_train_val_test_split(ds, k=5)
+
+    return DataManager(
+        ds,
+        train_indices=train_indices,
+        validation_indices=valid_indices,
+        test_indices=test_indices,
+        # FIXME
+        labelled_indices=pick_one_sample_per_class(ds, train_indices),
+        loader_batch_size="full",
+        loader_collate_fn=numpy_collate,
+    )
+
+
+def numpy_collate(
+    batch: List[Union[torch.Tensor, NDArray[Union[Any, np.float32, np.float64]]]]
+) -> List[NDArray[Union[Any, np.float32, np.float64]]]:
+    """Collate function for a Pytorch to Numpy DataLoader"""
+    return [np.stack([b.numpy() if isinstance(b, torch.Tensor) else b for b in samples]) for samples in zip(*batch)]
